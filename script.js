b083c5c8295d6523d0d2729b6b426b5c55f48b69
Chanwoo <gad006@naver.com>
2022-09-01 17:07:18 +0900

    2022.09.01

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 /* 여기부터 책의 코드를 작성해주세요 */
+
+const spanEl = document.querySelector("main h2 span");
+
+const txtArr=["Web Publisher", "Front-End Developer","Web UI Designer","UX Designer","Back-End Developer"];
+
+let index=0;
+
+let currentTxt=txtArr[index].split("");
+
+function writeTxt(){
+    spanEl.textContent+=currentTxt.shift();
+    if(currentTxt.length!==0){
+        setTimeout(writeTxt,Math.floor(Math.random()*100));
+    }
+    else{
+        currentTxt=spanEl.textContent.split("");
+        setTimeout(deleteTxt,3000);
+    }
+}
+
+writeTxt();
